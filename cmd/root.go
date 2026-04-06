8944f7094653af2ec1b3cdfcdb1053954c75ecf6
flynn <fdh3721@163.com>
2026-04-06 11:00:20 +0800

    add viper config fn

--- cmd/root.go
+++ cmd/root.go
@@ -4,31 +4,35 @@ Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"errors"
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "flynn-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+var (
+	// used for flags
+	cfgFile string
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
-}
+	// rootCmd represents the base command when called without any subcommands
+	rootCmd = &cobra.Command{
+		Use:   "flynnapp",
+		Short: "A brief description of your application",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			return initializeConfig(cmd)
+		},
+	}
+)
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
@@ -42,5 +46,43 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.myapp.yaml")
+}
+
+func initializeConfig(cmd *cobra.Command) error {
+	// 1. Set up Viper to use environment variables.
+	viper.SetEnvPrefix("flynn")
+	// Allow for nested keys in environment variables (e.g. `MYAPP_DATABASE_HOST`)
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "*", "_", "*"))
+	viper.AutomaticEnv()
+
+	// 2. Handle the configuration file.
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		viper.AddConfigPath(".")
+		viper.AddConfigPath(home + "/.flynn")
+		viper.SetConfigName("config")
+		viper.SetConfigType("yaml")
+	}
+
+	// 3. Read the configuration file.
+	if err := viper.ReadInConfig(); err == nil {
+		var configFileNotFoundError viper.ConfigFileNotFoundError
+		if !errors.As(err, &configFileNotFoundError) {
+			return err
+		}
+	}
+
+	// 4. Bind Cobra flags to Viper.
+	err := viper.BindPFlags(cmd.Flags())
+	if err != nil {
+		return err
+	}
+	return nil
 }
---
+++ cmd/serve.go
@@ -0,0 +1,46 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Start the server",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		port := viper.GetInt("port")
+		fmt.Printf("Starting server on port %d\n", port)
+		return nil
+	},
+	//Run: func(cmd *cobra.Command, args []string) {
+	//	fmt.Println("serve called")
+	//},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	serveCmd.Flags().IntP("port", "p", 8080, "Port to serve on")
+}
