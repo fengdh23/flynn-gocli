b16973c7d0f5292f1604bd6d0940ae670ce6a8d3
flynn <fdh3721@163.com>
2026-04-07 15:07:30 +0800

    custom cmd

--- cmd/root.go
+++ cmd/root.go
@@ -37,6 +37,8 @@ func Execute() {
 	}
 }
 
+var verbose bool
+
 func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
@@ -46,9 +48,10 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.myapp.yaml")
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func initializeConfig(cmd *cobra.Command) error {
--- cmd/serve.go
+++ cmd/serve.go
@@ -15,11 +15,7 @@ var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Start the server",
 	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+and usage of using your command. `,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		port := viper.GetInt("port")
 		fmt.Printf("Starting server on port %d\n", port)
